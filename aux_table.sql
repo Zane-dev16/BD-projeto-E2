ade0375f95eeb9acda2f271f6bbb781a9082cd26
Francisco Pinto <92582059+XaropCnora@users.noreply.github.com>
2024-05-31 01:11:58 +0100

    Update aux_table.sql

--- aux_table.sql
+++ aux_table.sql
@@ -5,17 +5,27 @@ CREATE TABLE horarios_validos (
     PRIMARY KEY (data, hora)
 );
 
-WITH RECURSIVE times AS (
+WITH RECURSIVE times_manha AS (
     SELECT TIME '08:00:00' AS time
     UNION ALL
     SELECT time + INTERVAL '30 minute'
-    FROM times
-    WHERE (time < '13:00:00') OR (time >= '14:00:00' AND time < '19:00:00')
+    FROM times_manha
+    WHERE time < '13:00:00'
+), times_tarde AS (
+    SELECT TIME '14:00:00' AS time
+    UNION ALL
+    SELECT time + INTERVAL '30 minute'
+    FROM times_tarde
+    WHERE time < '19:00:00'
 ), dates AS (
     SELECT generate_series('2024-01-01'::date, '2024-12-31'::date, INTERVAL '1 day') AS date
 )
 INSERT INTO horarios_validos (data, hora)
-SELECT dates.date, times.time
-FROM dates, times;
+SELECT dates.date, times_manha.time
+FROM dates, times_manha
+UNION ALL
+SELECT dates.date, times_tarde.time
+FROM dates, times_tarde;
 
-DROP TABLE IF EXISTS times;
+DROP TABLE IF EXISTS times_manha;
+DROP TABLE IF EXISTS times_tarde;
